d55048c3746c331c02b2bebab5e920f40ea78795
Marcos Lima <marcoslima749@gmail.com>
2023-05-31 00:42:16 -0300

    completa el ejercicio 5.5 del modulo 5

--- 5.Ciclos/5.5MaximoParesMinimoImpares/Program.cs
+++ 5.Ciclos/5.5MaximoParesMinimoImpares/Program.cs
@@ -3,14 +3,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         /*
 
         5. Hacer un programa que solicite 20 números y luego emitir por pantalla el máximo de los números pares y el mínimo de los números impares.
         
         */
 
-        
+        int c = 0;
+        int maximoPares = 0;
+        int minimoImpares = 0;
+
+        while (c<20)
+        {
+            Console.WriteLine("Ingrese un número:");
+            int numero = int.Parse(Console.ReadLine());
+            if(numero % 2 == 0) {
+                if(c == 0 ||  numero > maximoPares)
+                    maximoPares = numero;
+            } else {
+                if(c == 0 || numero < minimoImpares)
+                    minimoImpares = numero;
+            }
+
+            c++;
+        }
+
+        Console.WriteLine("El máximo de los pares es: " + maximoPares + " y el mínimo de los impares es: " + minimoImpares);
+
     }
 }
